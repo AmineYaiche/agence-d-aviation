fed222429b9f18502c4087945b74810634dea4e7
aminoss <amineyaich092@gmail.com>
2014-05-11 12:25:17 +0100

    correction d'une bug lors de l'inscription

--- model/inscription.php
+++ model/inscription.php
@@ -3,7 +3,7 @@ include_once "login_bd.php";
 
 function insert_user($row)
 {
-	$req= "INSERT INTO Utilisateur VALUES ('".$row['login']."','".$row['pwd']."', '";
+	$req= "INSERT INTO Utilisateur VALUES ('".$row['pseudo']."','".$row['mdp']."', '";
 	$req = $req.$row['nom']."','".$row['prenom']."','".$row['email']."' , 'non');";
 	mysql_query($req);
 }
