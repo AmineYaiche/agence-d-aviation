fec22c08781ee5cf99445e01a1b24210823f5a34
aminoss <amineyaich092@gmail.com>
2014-05-18 18:21:52 +0100

    ajout de validation des vol dans la page gestion_vol.php

--- admin/gestion_vol.php
+++ admin/gestion_vol.php
@@ -25,7 +25,7 @@ else
 {
 ?>
 <h2>Liste des vols</h2>
-<form method="POST" action="../controller/delete_vol.php">
+<form method="POST" action="../controller/gestion_vol.php">
 <table border=1>
 	<tr>
 		<th><label><input type="checkbox" onClick="selectAll(1,<?php echo $n; ?>)"/>
@@ -52,7 +52,9 @@ else
 	}
 
 ?><table/>
-	<input type="submit" value="Suppprimer"/>
+	<input type="submit" name="supprimer" value="Suppprimer"/>
+	<input type="submit" name="valider" value="Valider"/>
+
 <?php
 	$pres = (isset($_GET['page']) && $_GET['page']-1>=1)?$_GET['page']-1:1;
 	$suiv = (isset($_GET['page']))?$_GET['page']+1:2;
@@ -78,103 +80,7 @@ else
 	<input type="submit" value="Valider"/>
 </form>
 <!----------ajout d'un avion------------->
-<!------------------------------------ -->
-<?php /*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--liste des vol fait par un avion------>
-<h2>Liste des vols</h2>
-<form method="GET" action="#">
-identifiant avion : <input type="text" name="id_av"/><!-- il faut changer ça par un menu -->
-<input type="submit" value="Chercher"/><br/>
-</form>
-<?php
-if(!isset($_GET['id_av']))
-	echo "selectionner un avion.<br/>";
-else
-{?>
-<h2>Liste des vols effectué par l'avion : <?php $_GET['id_av'];?></h2>
-<?php
-$req = (isset($_GET['page_av']))?fetch_avion_vol($_GET['page_av']):fetch_avion_vol(1);
-if(!($n = mysql_num_rows($req)))
-{
-	if(id_exist("Avion" , "id_av" , $_GET['id_av']))
-	
-		echo "Pas de vol fait par cet avion.<br/>";
-	else
-		echo "identifiant inexistant dans la base.<br/>";
-	
-	if(isset($_GET['page_av']) && $_GET['page_av'] > 1)
-	{
-		$page = $_GET['page_av']-1;
-		header("location:gestion_avion.php?page_av=$page");
-	}
-	
-}
-else
-{
-?>
-<table border=1 >
-<tr>
-	<th>id vol</th>
-	<th>id avion</th>
-	<th>id equipe</th>
-	<th>date depart</th>
-	<th>depart</th>
-	<th>destination</th>
-	<th>date arrivé</th>
-	<th>place restant</th>
-	<th>valider</th>
-	<th>etat</th>
-</tr>
-<?php
-	for($i = 11 ; $row= mysql_fetch_assoc($req) ;$i++)
-	{
-		echo "<tr>";
-		foreach($row as $elem)
-			echo "<td>".$elem."</td>";
-		echo "</tr>";
-	}
-?>
-</table>
-<?php
-	$pres = (isset($_GET['page_av']) && $_GET['page_av']-1>=1)?$_GET['page_av']-1:1;
-	$suiv = (isset($_GET['page_av']))?$_GET['page_av']+1:2;
-	$page = (isset($_GET['page_av']))?$_GET['page_av']:1;
-	echo "<a href='?page_eq=$pres'> <= precedent</a> page ".$page." <a href='?page_eq=$suiv'>suivant => </a>";//il y a un bug ici
-}
-}?>
-
-<!--liste des vol fait par un avion------>
 <aside><?php include_once "../controller/aside.php";?></aside>
-<footer><?php include_once "../vue/footer.html";?></footer>*/?>
+<footer><?php include_once "../vue/footer.html";?></footer>
 </body>
 </html>
--- model/gestion_vol.php
+++ model/gestion_vol.php
@@ -6,3 +6,9 @@ function efface($vol)
 	$req = "DELETE FROM Vol WHERE id_vol = '$vol';";
 	mysql_query($req);
 }
+
+function valide($vol)
+{
+	$req = "UPDATE Vol SET valider = 'valider' WHERE id_vol = '$vol';";
+	mysql_query($req);
+}
